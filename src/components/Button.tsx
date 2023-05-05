53ed8595bd3c8cefbbfecd92071e504ab8a08868
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-05-06 02:07:02 +0900

    chore: vite 설정

---
+++ src/components/Button.tsx
@@ -0,0 +1,31 @@
+export interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({ backgroundColor, label, ...props }: ButtonProps) => (
+  <button type="button" style={{ backgroundColor }} {...props}>
+    {label}
+  </button>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
+import path from 'node:path';
+
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.tsx'),
+      name: 'MyLib',
+      formats: ['es', 'umd'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 });
