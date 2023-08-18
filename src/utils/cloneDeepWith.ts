57a2292504df4a17a33c6443770d9a5d1a1bb9c5
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-08-18 10:27:36 +0900

    docs(cloneDeepWith): 설명 작성
    - 불필요한 함수 제거

--- src/utils/cloneDeepWith.ts
+++ src/utils/cloneDeepWith.ts
@@ -1,5 +1,6 @@
 import { cloneDeep } from 'lodash-es';
 
+/** lodash의 cloneDeepWith이 내부의 값 수정이 외부에 영향을 미치는 문제가 있엇서 이를 해결하기 위해 만든 함수 */
 export const cloneDeepWith = <TOriginalValue, TCustomizerReturn>(
   value: TOriginalValue,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
