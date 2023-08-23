40965d996c84ee375cf88540c59cd8cfd385cce3
HyeokjaeLee <HyeokjaeLee@users.noreply.github.com>
2023-08-23 05:33:05 +0000

    deploy: 42df7b615e96e4d55e796eaee57623ce7c26d751

--- assets/syntaxhighlighter-4OVWVELQ-494c7d63.js
+++
@@ -1,2 +0,0 @@
-import{S as n,c as d,s as f}from"./DocsRenderer-3PUGWF3O-67eaba0e.js";import"./iframe-bb7daf74.js";import"../sb-preview/runtime.js";import"./index-61bf1805.js";import"./_commonjsHelpers-de833af9.js";import"./_getPrototype-d008cfdb.js";import"./index-2801d3c9.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{n as SyntaxHighlighter,d as createCopyToClipboardFunction,f as default};
-//# sourceMappingURL=syntaxhighlighter-4OVWVELQ-494c7d63.js.map
--- components/atoms/Switch/index.d.ts
+++ components/atoms/Switch/index.d.ts
@@ -1,7 +1,9 @@
+/// <reference types="react" />
 import { HTMLTagProps, InnerStateChangeEventHandler } from '@types';
 export interface SwitchProps extends Pick<HTMLTagProps<'div'>, 'className' | 'style'>, Omit<HTMLTagProps<'input'>, 'onChange' | 'size' | 'type' | 'style' | 'checked' | 'value'> {
     size?: 'small' | 'medium' | 'large';
     value?: boolean;
     onChange?: InnerStateChangeEventHandler<boolean>;
+    children?: React.ReactNode;
 }
-export declare const Switch: ({ size, onChange, value, className, style, ...restInputProps }: SwitchProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Switch: ({ size, onChange, value, children, className, style, ...restInputProps }: SwitchProps) => import("react/jsx-runtime").JSX.Element;
