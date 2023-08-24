be9bd9085654ed3ea094fd15f9cbd02c4ef443a5
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-08-25 02:14:24 +0900

    fix: 누락된 module export 추가
    - organisms 컴포넌트 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import './styles/_global.scss';
 
 export * from './components/atoms';
 export * from './components/molecules';
+export * from './components/organisms';
 
 export * from './hooks/public';
