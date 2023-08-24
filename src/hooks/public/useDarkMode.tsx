655670ef3ba3b2e788947d58a798b9dc760ea637
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-08-24 18:48:37 +0900

    feat: darkMode 개선
    - html class observer 추가

--- src/hooks/public/useDarkMode.tsx
+++ src/hooks/public/useDarkMode.tsx
@@ -1,23 +1,58 @@
+import { useEffect, useState } from 'react';
+
+enum MODE_TYPE {
+  LIGHT = 'pastime:light',
+  DARK = 'pastime:dark',
+}
+
 export const useDarkMode = () => {
-  const isDevicePrefersDarkMode = window.matchMedia(
+  const isDevicePrefersDarkMode = window?.matchMedia(
     '(prefers-color-scheme: dark)',
   ).matches;
-  const hasDarkModeClass =
-    document.documentElement.classList.contains('pastime:dark');
-  const hasLightModeClass =
-    document.documentElement.classList.contains('pastime:light');
+
+  const [isDarkMode, setIsDarkMode] = useState(isDevicePrefersDarkMode);
+
+  useEffect(() => {
+    const html = document?.documentElement;
+
+    if (!html) return;
+
+    const setDarkMode = () => {
+      const { classList } = html;
+
+      const isDarkMode =
+        !classList.contains(MODE_TYPE.LIGHT) &&
+        (classList.contains(MODE_TYPE.DARK) || isDevicePrefersDarkMode);
+
+      setIsDarkMode(isDarkMode);
+    };
+
+    setDarkMode();
+
+    const darkModeObserver = new MutationObserver((mutations) => {
+      mutations.forEach(({ attributeName }) => {
+        if (attributeName === 'class') setDarkMode();
+      });
+    });
+
+    darkModeObserver.observe(html, {
+      attributes: true,
+      attributeFilter: ['class'],
+    });
+
+    return () => darkModeObserver.disconnect();
+  }, [isDevicePrefersDarkMode]);
 
   return {
-    isDarkMode:
-      !hasLightModeClass && (hasDarkModeClass || isDevicePrefersDarkMode),
+    isDarkMode,
 
     setDarkMode: (isDarkMode: boolean) => {
       if (isDarkMode) {
-        document.documentElement.classList.add('pastime:dark');
-        document.documentElement.classList.remove('pastime:light');
+        document.documentElement.classList.add(MODE_TYPE.DARK);
+        document.documentElement.classList.remove(MODE_TYPE.LIGHT);
       } else {
-        document.documentElement.classList.add('pastime:light');
-        document.documentElement.classList.remove('pastime:dark');
+        document.documentElement.classList.add(MODE_TYPE.LIGHT);
+        document.documentElement.classList.remove(MODE_TYPE.DARK);
       }
     },
   };
