8bcf6c9ea60f8c08912f4811632e7320f40776a8
HyeokjaeLee <HyeokjaeLee@users.noreply.github.com>
2023-08-30 09:58:00 +0000

    deploy: 5c58eb5df3f8bd0d516a8745d90d23175ea92910

---
+++ assets/index.stories-c82ef389.js
@@ -0,0 +1,15 @@
+import{j as o}from"./jsx-runtime-4ca860c5.js";import{r as d}from"./index-61bf1805.js";import{o as p,i as m,h,k as E,H as r,j as x,F as f}from"./useGetter-f42a888b.js";import{O as v}from"./STORY_DUMMY-1d44b6df.js";import"./_commonjsHelpers-de833af9.js";import"./index-6831fd36.js";import"./index-8d47fad6.js";import"./index-2801d3c9.js";const j={title:"atoms/Select",component:p,argTypes:{opened:{control:"boolean",description:`Whether it's open or not.
+
+열려있는지 여부`},float:m,options:h,multiple:E,onChange:r,onKeyDown:r,cancelable:x,fixedDarkMode:f,value:{description:`The value of the selected option.
+
+선택된 옵션의 값`}},args:{opened:!0,options:v,float:"bottom",multiple:!1,cancelable:!0},decorators:[e=>o.jsx("article",{style:{position:"relative",height:650,padding:"10px 0"},children:o.jsx("div",{style:{margin:"300px auto",position:"relative",width:"400px"},children:o.jsx(e,{})})})]},t={render:e=>{const[u,c]=d.useState();return o.jsx(p,{...e,value:u,onChange:a=>{var n;c(a.value),(n=e.onChange)==null||n.call(e,a)}})}};var s,l,i;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: props => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [value, setValue] = useState<ValidOptionValue | ValidOptionValue[]>();
+    return <Select {...props} value={value} onChange={event => {
+      setValue(event.value);
+      props.onChange?.(event);
+    }} />;
+  }
+}`,...(i=(l=t.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};const V=["Default"];export{t as Default,V as __namedExportsOrder,j as default};
+//# sourceMappingURL=index.stories-c82ef389.js.map
