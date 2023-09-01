dd84ee059d14bff09c01ea8c26bd3a9eaed1e21c
HyeokjaeLee <HyeokjaeLee@users.noreply.github.com>
2023-09-01 02:35:35 +0000

    deploy: a1c4bb1856b41cb1b11c98ea2baca2fdf44b3c9a

--- hooks/public/useValidate.d.ts
+++ hooks/public/useValidate.d.ts
@@ -8,7 +8,7 @@ interface ValidateOptions {
 }
 export declare const useValidate: () => {
     validationMap: Map<string, () => import('../../contexts/ValidationContext').ValidationResult | Promise<import('../../contexts/ValidationContext').ValidationResult>>;
-    validate: ({ scroll }: ValidateOptions) => ValidateResult;
+    validate: (params?: ValidateOptions) => ValidateResult;
     validateAsync: ({ scroll, }: ValidateOptions) => Promise<ValidateResult>;
 };
 export declare const validationObserver: <T extends object>(Component: ComponentType<T>) => (props: T) => import("react/jsx-runtime").JSX.Element;
