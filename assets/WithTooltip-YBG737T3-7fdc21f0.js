68ba2c6fb9567fe50a8314ada3e39b73e3ef2b99
HyeokjaeLee <HyeokjaeLee@users.noreply.github.com>
2023-09-05 16:20:03 +0000

    deploy: 454fc4712bcbe8911567fe6f45ece40a914e496b

--- assets/WithTooltip-YBG737T3-7fdc21f0.js
+++
@@ -1,2 +0,0 @@
-import{W as l,W as s,a as f}from"./DocsRenderer-3PUGWF3O-49f2d0b6.js";import"./iframe-30114389.js";import"../sb-preview/runtime.js";import"./index-61bf1805.js";import"./_commonjsHelpers-de833af9.js";import"./_getPrototype-d008cfdb.js";import"./index-2801d3c9.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{l as WithToolTipState,s as WithTooltip,f as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-YBG737T3-7fdc21f0.js.map
--- hooks/public/useDarkMode.d.ts
+++ hooks/public/useDarkMode.d.ts
@@ -1,5 +1,5 @@
 export type FixedDarkMode = 'light' | 'dark';
 export declare const useDarkMode: (fixedDarkMode?: FixedDarkMode) => {
     isDarkMode: boolean;
-    setDarkMode: (isDarkMode: boolean) => void;
+    setIsDarkMode: (isDarkMode: boolean) => void;
 };
