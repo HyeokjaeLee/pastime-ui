52ce4ac3f71fd77735bae69401e8ea11145572c0
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-09-20 17:23:12 +0900

    fix: 딜레이 버튼 버그 수정
    - 일부 환경에서 정상적으로 progress bar가 진행되지 않는 문제 해결
    - 버튼 크기 설정 height > padding 값으로 변경

--- src/hooks/private/useButtonDelay.tsx
+++ src/hooks/private/useButtonDelay.tsx
@@ -5,6 +5,8 @@ interface UseButtonDelayParams {
   disabled?: boolean;
 }
 
+const HEAP_TIMER = 500;
+
 export const useButtonDelay = ({ delay, disabled }: UseButtonDelayParams) => {
   const [delayState, setDelayState] = useState<'before' | 'delaying' | 'after'>(
     'after',
@@ -17,12 +19,18 @@ export const useButtonDelay = ({ delay, disabled }: UseButtonDelayParams) => {
   useEffect(() => {
     switch (delayState) {
       case 'before': {
-        const delayTimer = setTimeout(() => setDelayState('delaying'));
+        const delayTimer = setTimeout(
+          () => setDelayState('delaying'),
+          HEAP_TIMER,
+        );
         return () => clearTimeout(delayTimer);
       }
 
       case 'delaying': {
-        const endTimer = setTimeout(() => setDelayState('after'), delay);
+        const endTimer = setTimeout(
+          () => setDelayState('after'),
+          delay ?? 0 + HEAP_TIMER,
+        );
         return () => {
           clearTimeout(endTimer);
         };
